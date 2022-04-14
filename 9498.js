6ac17fa17594eecc972a6b02f7b1c2669592225e
WaiNaat <qoli0703@gmail.com>
2022-04-14 12:48:51 +0900

    if문
    제출할 때 언어 node.js로 꼭 바꾸기

---
+++ 9498.js
@@ -0,0 +1,13 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+const score = parseInt(input);
+
+if (score >= 90)
+    console.log('A');
+else if (score >= 80)
+    console.log('B');
+else if (score >= 70)
+    console.log('C');
+else if (score >= 60)
+    console.log('D');
+else
+    console.log('F');
