7c3683fd448ac834835f9175b5f41ce3644c4222
Q Kim <qoli0703@gmail.com>
2022-11-17 14:14:11 +0900

    구현
    array.splice(시작점, 지울개수, 넣을값들);

---
+++ 1138_v2.js
@@ -0,0 +1,23 @@
+/*
+큰 숫자부터 배열에 배치한다.
+본인 앞에 본인보다 큰 i명이 있다고 치면 배열[i] 에 넣으면 된다.
+max(N)=10이라 배열에 넣을 때의 시간 복잡도 최적화할 필요 X
+*/
+// input
+const inputFile = process.platform === 'linux'? '/dev/stdin' : './input';
+const [N, ...tallerThanMe] = require('fs').readFileSync(inputFile).toString().trim()
+  .split(/\s/)
+  .map(Number);
+
+// process
+const order = tallerThanMe.reduceRight(
+  (prevArray, number, index) => {
+    prevArray.splice(number, 0, index + 1);
+    return prevArray;
+  },
+  [undefined],
+);
+order.pop();
+
+// output
+console.log(order.join(' '));
