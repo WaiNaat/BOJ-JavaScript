3c68ace778987d898add2b58c8eb86f6c51f43b1
WaiNaat <qoli0703@gmail.com>
2024-05-30 22:32:36 +0900

    구현

---
+++ 27466.js
@@ -0,0 +1,23 @@
+// 아 v-flag 지원안해주네 ㅋㅋ
+
+const INPUT_FILE = process.platform === 'linux' ? '/dev/stdin' : './input';
+const [, targetLengthString, inputString] = require('fs')
+  .readFileSync(INPUT_FILE)
+  .toString()
+  .trim()
+  .split(/\s/);
+
+const targetLength = Number(targetLengthString);
+const regex = /^(?<prefix>.*)A.*A(?<suffix>[[A-Z]--[AEIOU]]).*$/v;
+const matchResult = inputString.match(regex);
+const result =
+  matchResult !== null
+    ? `${matchResult.groups.prefix.slice(-targetLength + 3)}AA${matchResult.groups.suffix}`
+    : '';
+
+if (result.length !== targetLength) {
+  console.log('NO');
+} else {
+  console.log('YES');
+  console.log(result);
+}
